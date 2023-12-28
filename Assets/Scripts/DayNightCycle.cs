2a754bdca9770ded6d76be1c44ff48e7450f2159
Matan Zvulun <48678786+YetItMoves-MZ@users.noreply.github.com>
2023-12-29 00:57:37 +0200

    added day night cycle and a clock to show time.

---
+++ Assets/Scripts/DayNightCycle.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+    public delegate void MidDayFunctions();
+    public MidDayFunctions DayFunctions;
+    public MidDayFunctions NightFunctions;
+    public Transform DirectionalLight;
+    public float TimeInSecondsForFullDay;
+    public TMPro.TMP_Text Clock;
+
+    float currentTime;
+    bool pastNightTime; // prevent multiple night/day functions from calling more than once each time.
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = TimeInSecondsForFullDay;
+        DirectionalLight.rotation = Quaternion.Euler((currentTime / (TimeInSecondsForFullDay)) * 360f, 0, 0);
+        pastNightTime = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        DirectionalLight.rotation = Quaternion.Euler((currentTime / (TimeInSecondsForFullDay)) * 360f, 0, 0);
+
+        if (currentTime >= TimeInSecondsForFullDay / 2 && !pastNightTime)
+            Night();
+        else if (currentTime >= TimeInSecondsForFullDay)
+            Day();
+
+        Clock.text = PersentageToClock(currentTime / TimeInSecondsForFullDay, 6);
+    }
+
+    void Night()
+    {
+        pastNightTime = true;
+        print("night");
+        NightFunctions?.Invoke();
+    }
+
+    void Day()
+    {
+        currentTime -= TimeInSecondsForFullDay;
+        pastNightTime = false;
+        print("day");
+        DayFunctions?.Invoke();
+
+    }
+
+    string PersentageToClock(float persentage, int hourOffset = 0)
+    {
+        float hours = persentage * 24;
+        hours = (hours + hourOffset) % 24;
+        float minutes = ((hours % 24) * 60) % 60;
+
+        return string.Format("{0:D2}", (int)hours) + ":" + string.Format("{0:D2}", (int)minutes);
+    }
+
+    public void SkipDay()
+    {
+        currentTime = TimeInSecondsForFullDay / 2;
+    }
+}
