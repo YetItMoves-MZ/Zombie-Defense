ed42eba93c839e30c9849aed7a0d730b303c2751
Matan Zvulun <48678786+YetItMoves-MZ@users.noreply.github.com>
2024-01-02 14:52:14 +0200

    Added turrets prefabs, added health bar slider

--- Assets/Scripts/BuildingValidator.cs
+++ Assets/Scripts/BuildingValidator.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class BuildingValidator : MonoBehaviour
 {
-    [SerializeField] MeshRenderer mesh;
+    // [SerializeField] MeshRenderer mesh;
     List<Collider> blockingColliders;
 
     void Awake()
@@ -15,15 +16,17 @@ public class BuildingValidator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        mesh.material.color = Color.red;
-        BuildingHandler.Instance.IsInBuildableLocation = false;
+        // mesh.material.color = Color.red;
+        MakeSolidColor(Color.green, transform);
+        BuildingHandler.Instance.IsInBuildableLocation = true;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
             return;
-        mesh.material.color = Color.red;
+        // mesh.material.color = Color.red;
+        MakeSolidColor(Color.red, transform);
         BuildingHandler.Instance.IsInBuildableLocation = false;
         blockingColliders.Add(other);
     }
@@ -34,7 +37,20 @@ public class BuildingValidator : MonoBehaviour
         blockingColliders.Remove(other);
         if (blockingColliders.Count != 0)
             return;
-        mesh.material.color = Color.green;
+        // mesh.material.color = Color.green;
+        MakeSolidColor(Color.green, transform);
         BuildingHandler.Instance.IsInBuildableLocation = true;
+    }
+
+    void MakeSolidColor(Color color, Transform transform)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            MakeSolidColor(color, transform.GetChild(i));
+        }
+        if (transform.TryGetComponent(out MeshRenderer mesh))
+        {
+            mesh.material.color = color;
+        }
     }
 }
